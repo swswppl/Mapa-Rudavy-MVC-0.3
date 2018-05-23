1050576629611c9e17a1e330d185ddecf2afbeec
swswppl <sws@wp.pl>
2018-05-23 18:32:45 +0200

    Add project files.

---
+++ Mapa Rudavy MVC 0.3/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Mapa_Rudavy_MVC_0._3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Mapa Rudavy MVC 0.3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mapa_Rudavy_MVC_0._3.MvcApplication" Language="C#" %>
